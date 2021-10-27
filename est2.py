b5ce63e278ac0b65787971ddf55099c7ac965f27
Tan <tanvo149@outlook.com>
2021-10-27 19:30:53 +0100

    first commit

---
+++ est2.py
@@ -0,0 +1,9 @@
+#import yahoo_fin.stock_info as si    
+
+#dow = si.tickers_dow()
+#print(dow)
+
+#day_gainer = si.get_day_most_active()
+#print(day_gainer)
+
+#get_data(ticker, start_date, end_date, interval="1d")

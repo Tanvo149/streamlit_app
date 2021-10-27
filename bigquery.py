113e0bdbaa25cfddd8fc417171b86c127735ad76
Tan Vo <tanvo149@outlook.com>
2021-10-27 19:58:37 +0100

    first comiittt

--- bigquery.py
+++ bigquery.py
@@ -2,6 +2,7 @@ import streamlit as st
 from google.oauth2 import service_account
 from google.cloud import bigquery
 
+#why ..
 # Create API client.
 credentials = service_account.Credentials.from_service_account_info(
     st.secrets["gcp_service_account"]
